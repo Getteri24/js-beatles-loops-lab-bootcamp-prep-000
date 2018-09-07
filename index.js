68aab9ada3e64ee87c402ba4dde21fb8fb4e4bbb
t bryant <trbryant1981@gmail.com>
2018-09-07 14:04:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ function theBeatlesPlay(musicians, instrument) {
   return theBandInstruments;
 }
 
-function johnLennonFacts(array) {
-  let facts = ['He was the last Beatle to learn to drive', 'He was never a vegetarian', 'He was a choir boy and boy scout', 'He hated the sound of his own voice'];
+function johnLennonFacts(facts) {
   let newFacts = [];
+  let i = 0;
   while(i < facts.length) {
-    facts[i].length + '!!!';
+    newFacts.push(facts[i] + "!!!");
+    i++;
   }
-  return facts;
+  return newFacts;
 }
